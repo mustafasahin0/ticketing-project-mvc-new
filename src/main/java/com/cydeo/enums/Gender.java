9e4ed96a06af9dc5f925ddaca59ffa205f28fd99
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-03 20:03:23 -0600

    added base entity and user classes

---
+++ src/main/java/com/cydeo/enums/Gender.java
@@ -0,0 +1,14 @@
+package com.cydeo.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+    MALE("Male"), FEMALE("Female");
+    private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+}
+
