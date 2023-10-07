4592a2481192d2a387a8b703bf39bab0cc407b26
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-07 12:31:44 -0600

    part 2

--- src/main/java/com/cydeo/service/impl/AbstractMapService.java
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -1,6 +1,28 @@
 package com.cydeo.service.impl;
 
-public abstract class AbstractMapService {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    protected Map<ID, T>
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+    T save(ID id, T object) {
+        map.put(id, object);
+        return object;
+    }
+
+    List<T> findAll() {
+        return new ArrayList<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    void deleteById(ID id){
+        map.remove(id);
+    }
 }
