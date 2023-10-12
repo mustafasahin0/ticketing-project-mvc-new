48cedf6eac12f2a909ecb8f141abc91e97d14276
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-10-11 23:22:20 -0600

    part 4

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/task")
 public class TaskController {
 
+    ProjectService projectService;
+    UserService userService;
+
+    public TaskController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
-    public String createTask(){
+    public String createTask(Model model){
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("employees", userService.managers());
         return "/task/create";
     }
 }
